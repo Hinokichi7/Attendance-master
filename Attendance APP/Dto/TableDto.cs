031a3b897cfe8cae6245002b4e5af354fa096848
HarukaMori <faeriepunks7610@gmail.com>
2021-01-03 10:25:09 +0900

    1/3

--- Attendance APP/Dto/TableDto.cs
+++ Attendance APP/Dto/TableDto.cs
@@ -8,69 +8,15 @@ namespace Attendance_APP
 {
     class TableDto
     {
-        public string Department { get; private set; }
-        public string Employee { get; private set; }
-        public string Year { get; private set; }
-        public string Month { get; private set; }
-        public string Day { get; private set; }
-        public string Attendance { get; private set; }
-        public string LeavingWork { get; private set; }
-        public string StampingType { get; private set; }
-        public string WorkingHours { get; private set; }
-        public string Remark { get; private set; }
-
-        public TableDto(string department, string employee, string year, string month, string day, string attendance, string leavingWork, string stampingType, string workingHours, string remark)
-        {
-            this.Department = department;
-            this.Employee = employee;
-            this.Year = year;
-            this.Month = month;
-            this.Day = day;
-            this.Attendance = attendance;
-            this.LeavingWork = leavingWork;
-            this.StampingType = stampingType;
-            this.WorkingHours = workingHours;
-            this.Remark = remark;
-        }
-
-        public override int GetHashCode()
-        {
-            return 
-                Department.GetHashCode() ^
-                Employee.GetHashCode() ^
-                Year.GetHashCode() ^
-                Month.GetHashCode() ^
-                Day.GetHashCode() ^
-                Attendance.GetHashCode() ^
-                LeavingWork.GetHashCode() ^
-                StampingType.GetHashCode() ^
-                WorkingHours.GetHashCode() ^
-                Remark.GetHashCode();
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null || this.GetType() != obj.GetType())
-            {
-                return false;
-            }
-            else
-            {
-                TableDto table = (TableDto)obj;
-                return (
-                    this.Department == table.Department &&
-                    this.Employee == table.Employee &&
-                    this.Year == table.Year &&
-                    this.Month == table.Month &&
-                    this.Day == table.Day &&
-                    this.Attendance == table.Attendance &&
-                    this.LeavingWork == table.LeavingWork &&
-                    this.StampingType == table.StampingType &&
-                    this.WorkingHours == table.WorkingHours &&
-                    this.Remark == table.Remark
-                    );
-
-            }
-        }
+        public string Department { get; set; }
+        public string Employee { get; set; }
+        public string Year { get; set; }
+        public string Month { get; set; }
+        public string Day { get; set; }
+        public string Attendance { get; set; }
+        public string LeavingWork { get; set; }
+        public string StampingType { get; set; }
+        public string WorkingHours { get; set; }
+        public string Remark { get; set; }
     }
 }
---
+++ Attendance APP/DtoKeys.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attendance_APP
+{
+    class DtoKeys
+    {
+        public string Department { get; private set; }
+        public string Employee { get; private set; }
+        public string Stamping { get; private set; }
+        public string StampingType { get; private set; }
+        public string Table { get; private set; }
+
+        public DtoKeys(string department, string employee, string stamping, string stampingType, string table)
+        {
+            this.Department = department;
+            this.Employee = employee;
+            this.Stamping = stamping;
+            this.StampingType = stampingType;
+            this.Table = table;
+        }
+
+        public override int GetHashCode()
+        {
+            return
+                Department.GetHashCode() ^
+                Employee.GetHashCode() ^
+                Stamping.GetHashCode() ^
+                StampingType.GetHashCode() ^
+                Table.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+            {
+                return false;
+            }
+            else
+            {
+                DtoKeys keys = (DtoKeys)obj;
+                return (
+                    this.Department == keys.Department &&
+                    this.Employee == keys.Employee &&
+                    this.Stamping == keys.Stamping &&
+                    this.StampingType == keys.StampingType &&
+                    this.Table == keys.Table
+                    );
+            }
+        }
+    }
+}
