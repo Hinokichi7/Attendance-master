455e7f62335907d5e7d379efe4fb529d2ba5ec1c
mori <haruka_mori@fix-inc.co.jp>
2021-01-06 15:08:16 +0900

    1/6 5

--- Attendance APP/Util/EmployeeDictionary.cs
+++ Attendance APP/Util/EmployeeDictionary.cs
@@ -19,6 +19,11 @@ namespace Attendance_APP
                 new EmployeeKeys(1, "岡崎 誠", 1,"aaa", 1),
                 new EmployeeKeys(2, "永瀬 昌利", 2, "bbb", 0),
                 new EmployeeKeys(3,"池田 沙菜", 3, "ccc", 0),
+                new EmployeeKeys(4,"渡辺 敏男", 4,"ddd", 1),
+                new EmployeeKeys(5, "森田 利勝", 5, "eee", 0),
+                new EmployeeKeys(6, "末永 好男", 1, "fff", 1),
+                new EmployeeKeys(7, "尾上 千尋", 2, "ggg", 0),
+                new EmployeeKeys(8, "大槻 龍宏", 3, "hhh", 0),
 
             };
 
@@ -29,6 +34,14 @@ namespace Attendance_APP
                 { employeeKeys[0], employees[0] },
                 { employeeKeys[1], employees[1] },
                 { employeeKeys[2], employees[2]},
+                { employeeKeys[3], employees[3]},
+                { employeeKeys[4], employees[4]},
+                { employeeKeys[5], employees[5]},
+                { employeeKeys[6], employees[6]},
+                { employeeKeys[7], employees[7]},
+                { employeeKeys[8], employees[8]},
+                { employeeKeys[9], employees[9]},
+                { employeeKeys[10], employees[10]},
             };
         }
 
